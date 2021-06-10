38f799565b3056451a141913d46cbb5e6b5ca8a2
Alisson Bruno <alissonbruno.2015@hotmail.com>
2021-06-09 21:22:07 -0300

    Ultima aula, 'Componente Obter Produto'.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,14 +14,26 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { HomeComponent } from './views/home/home.component';
+import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { RedDirective } from './directives/red.directive';
+import { ForDirective } from './directives/for.directive';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { HttpClientModule } from '@angular/common/http';
 
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from  '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+ 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent, 
-    NavComponent, HomeComponent
+    NavComponent, HomeComponent, ProductCrudComponent, RedDirective, ForDirective, ProductCreateComponent, ProductReadComponent
   ],
   imports: [
     BrowserModule,
@@ -30,7 +42,13 @@ import { HomeComponent } from './views/home/home.component';
     MatToolbarModule,
     MatSidenavModule,
     MatListModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    HttpClientModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
